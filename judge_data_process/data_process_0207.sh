20a3f65806cde49a50a421b377c233f6800fb81a
WeiLiuAH <88260763+WeiLiuAH@users.noreply.github.com>
2025-02-20 18:35:37 +0800

    Update data_process_0207.sh

--- judge_data_process/data_process_0207.sh
+++ judge_data_process/data_process_0207.sh
@@ -2,7 +2,7 @@ model_name=llama3_ultrachat
 input_path=./data/llm_as_a_judge_${model_name}.jsonl
 
 
-python3 data_generate.py \
+python ./judge_data_process/data_generate.py \
     --input_path ${input_path} \
     --loop1_path "./data/${model_name}/loop1_guidance.json" \
     --loop2_path "./data/${model_name}/loop2_guidance.json" \
